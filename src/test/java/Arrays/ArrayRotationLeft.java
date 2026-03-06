6445ee7ffa7e16506dfe99e9a9672552f8c05857
Akshay Sunil Pawar <akshay.10852273@ltimindtree.com>
2026-03-06 13:48:37 +0530

    Array Left Right logic

---
+++ src/test/java/Arrays/ArrayRotationLeft.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class ArrayRotationLeft {
+    public static void main(String[] args) {
+
+        int arr [] = {1,2,3,4,5,6};
+
+        int first = arr[0];
+
+        for (int i = 0; i<arr.length-1; i++ ){
+             arr[i]= arr[i+1];
+        }
+
+        arr[arr.length-1] = first;
+
+        for (int num : arr){
+            System.out.print(num);
+        }
+    }
+
+}
